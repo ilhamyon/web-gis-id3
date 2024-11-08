1b01a56d89e276f39648359e80b5b7313c9e857f
ilhamyon <ilhamyon@gmail.com>
2024-11-08 13:51:02 +0700

    add project

---
+++ data/data_kunjungan.js
@@ -0,0 +1,41 @@
+var json_data_kunjungan = 
+{
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 1,
+        "namaRelawan": "Zulkifli",
+        "alamatTujuan": "Desa Labulia",
+        "namaYangDikunjungi": "M Andrian",
+        "keteranganKunjungan": "Penyuluhan",
+        "foto": "Foto/SMKN 1 Kota Malang.jpeg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          116.1891142,
+          -8.6860711
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": 2,
+        "namaRelawan": "Herdian",
+        "alamatTujuan": "Desa Nyerot",
+        "namaYangDikunjungi": "Muhsinin",
+        "keteranganKunjungan": "Penyuluhan",
+        "foto": "Foto_SMK PGRI 2 Malang.jpeg"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          116.2275085,
+          -8.6807382
+        ]
+      }
+    }
+  ]
+}
